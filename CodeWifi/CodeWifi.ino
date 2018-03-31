960fc45a3ec65a011bad4a183ac73bc010a99593
ThanhSon <thanhsoncun@gmail.com>
2018-03-31 15:37:39 +0700

    init project

---
+++ CodeWifi/CodeWifi.ino
@@ -0,0 +1,89 @@
+
+#include <SocketIOClient.h>
+#include <ESP8266WiFi.h>
+
+extern String RID;
+extern String Rfull;
+ 
+ 
+//Một số biến dùng cho việc tạo một task
+unsigned long previousMillis = 0;
+long interval = 2000;
+
+SocketIOClient client;
+const char* ssid = "ASUS_Z00VD";          //Tên mạng Wifi mà Socket server của bạn đang kết nối
+const char* password = "1122334455";  //Pass mạng wifi ahihi, anh em rãnh thì share pass cho mình với.
+ 
+char host[] = "192.168.43.174";  //Địa chỉ IP dịch vụ, hãy thay đổi nó theo địa chỉ IP Socket server của bạn.
+int port = 3484;                  //Cổng dịch vụ socket server do chúng ta tạo!
+ 
+//từ khóa extern: dùng để #include các biến toàn cục ở một số thư viện khác. Trong thư viện SocketIOClient có hai biến toàn cục
+// mà chúng ta cần quan tâm đó là
+// RID: Tên hàm (tên sự kiện
+// Rfull: Danh sách biến (được đóng gói lại là chuối JSON)
+ 
+void setup()
+{
+    //Bật baudrate ở mức 115200 để giao tiếp với máy tính qua Serial
+    Serial.begin(115200);
+    delay(10);
+//    pinMode(A0,INPUT);
+    pinMode(D0,INPUT);
+    pinMode(D1,OUTPUT);
+    //Việc đầu tiên cần làm là kết nối vào mạng Wifi
+    Serial.print("Ket noi vao mang ");
+    Serial.println(ssid);
+ 
+    //Kết nối vào mạng Wifi
+    WiFi.begin(ssid, password);
+ 
+    //Chờ đến khi đã được kết nối
+    while (WiFi.status() != WL_CONNECTED) { //Thoát ra khỏi vòng 
+        delay(500);
+        Serial.print('.');
+    }
+ 
+    Serial.println();
+    Serial.println(F("Da ket noi WiFi"));
+    Serial.println(F("Di chi IP cua ESP8266 (Socket Client ESP8266): "));
+    Serial.println(WiFi.localIP());
+ 
+    if (!client.connect(host, port)) {
+        Serial.println(F("Ket noi den socket server that bai!"));
+        return;
+    }
+ 
+    //Khi đã kết nối thành công
+    if (client.connected()) {
+        //Thì gửi sự kiện ("connection") đến Socket server ahihi.
+        client.send("connection", "message", "Connected !!!!");
+    }
+}
+ 
+void loop()
+{
+    int a = digitalRead(D0);
+    String s = String (a);
+    if (a == 1 ) {
+      digitalWrite(D1,1);
+    } else {
+      digitalWrite(D1,0);
+    }
+    client.send("data", s);
+    
+    delay (1000);
+ 
+    //Khi bắt được bất kỳ sự kiện nào thì chúng ta có hai tham số:
+    //  +RID: Tên sự kiện
+    //  +RFull: Danh sách tham số được nén thành chuỗi JSON!
+    if (client.monitor()) {
+        Serial.println(RID);
+        Serial.println(Rfull);
+    }
+ 
+    //Kết nối lại!
+    if (!client.connected()) {
+      client.reconnect(host, port);
+    }
+}
+
---
+++ socketServer1-master/index.js
@@ -0,0 +1,24 @@
+const PORT = 3484;									//Đặt địa chỉ Port được mở ra để tạo ra chương trình mạng Socket Server
+ 
+var http = require('http') 							//#include thư viện http - Tìm thêm về từ khóa http nodejs trên google nếu bạn muốn tìm hiểu thêm. Nhưng theo kinh nghiệm của mình, Javascript trong môi trường NodeJS cực kỳ rộng lớn, khi bạn bí thì nên tìm hiểu không nên ngồi đọc và cố gắng học thuộc hết cái reference (Tài liêu tham khảo) của nodejs làm gì. Vỡ não đó!
+var socketio = require('socket.io')				//#include thư viện socketio
+ 
+var ip = require('ip');
+var app = http.createServer();					//#Khởi tạo một chương trình mạng (app)
+var io = socketio(app);								//#Phải khởi tạo io sau khi tạo app!
+app.listen(PORT);										// Cho socket server (chương trình mạng) lắng nghe ở port 3484
+console.log("Server nodejs chay tai dia chi: " + ip.address() + ":" + PORT)
+ 
+//Khi có mệt kết nối được tạo giữa Socket Client và Socket Server
+io.on('connection', function(socket) {	
+    console.log("Connected"); //In ra màn hình console là đã có một Socket Client kết nối thành công.
+	socket.on('data', function(data){
+		console.log(data);
+		io.emit('serverSendData',data);
+	});
+	//Khi socket client bị mất kết nối thì chạy hàm sau.
+	socket.on('disconnect', function() {
+		console.log("disconnect") 	//in ra màn hình console cho vui
+		clearInterval(interval1)		//xóa chu kỳ nhiệm vụ đi, chứ không xóa là cái task kia cứ chạy mãi thôi đó!
+	})
+});
