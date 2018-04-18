05ea2abd1ef6fe38fc4a6d413e3ca5ab1f142c6f
ThanhSon <thanhsoncun@gmail.com>
2018-04-18 20:50:25 +0700

    Updat

--- CodeArduino/CodeArduino.ino
+++ CodeArduino/CodeArduino.ino
@@ -6,7 +6,7 @@ const char* ssid = "ASUS_Z00VD";          //Tên mạng Wifi mà Socket server c
 const char* password = "1122334455";  //Pass mạng wifi ahihi, anh em rãnh thì share pass cho mình với.
  
 char host[] = "192.168.43.174";  //Địa chỉ IP dịch vụ, hãy thay đổi nó theo địa chỉ IP Socket server của bạn.
-int port = 3484;                  //Cổng dịch vụ socket server do chúng ta tạo!
+int port = 3000;                  //Cổng dịch vụ socket server do chúng ta tạo!
  
 //từ khóa extern: dùng để #include các biến toàn cục ở một số thư viện khác. Trong thư viện SocketIOClient có hai biến toàn cục
 // mà chúng ta cần quan tâm đó là
@@ -14,7 +14,8 @@ int port = 3484;                  //Cổng dịch vụ socket server do chúng t
 // Rfull: Danh sách biến (được đóng gói lại là chuối JSON)
 extern String RID;
 extern String Rfull;
- 
+bool kt = false;
+int gt; 
  
 //Một số biến dùng cho việc tạo một task
 unsigned long previousMillis = 0;
@@ -26,6 +27,7 @@ void setup()
     Serial.begin(115200);
     delay(10);
     pinMode(A0,INPUT);
+    pinMode(D0,OUTPUT);
     //Việc đầu tiên cần làm là kết nối vào mạng Wifi
     Serial.print("Ket noi vao mang ");
     Serial.println(ssid);
@@ -58,10 +60,23 @@ void setup()
  
 void loop()
 {
-    int a = analogRead(A0);
+   int a = analogRead(A0);
+    if (kt == false) {
+      gt = a;
+      kt = true;
+      Serial.println("-------------");
+      Serial.println(gt);
+    }
+
     String s = String (a);
     Serial.println(a);
-   client.send("data", s);
+    if (a >= gt+10) {
+      client.send("data", s);
+      digitalWrite(D0,1);
+    } else {
+      digitalWrite(D0,0);
+    }
+    delay (1000);
  
     //Khi bắt được bất kỳ sự kiện nào thì chúng ta có hai tham số:
     //  +RID: Tên sự kiện
--- socketServer1-master/index.js
+++ socketServer1-master/index.js
@@ -1,4 +1,4 @@
-const PORT = 3484;									//Đặt địa chỉ Port được mở ra để tạo ra chương trình mạng Socket Server
+const PORT = 3000;									//Đặt địa chỉ Port được mở ra để tạo ra chương trình mạng Socket Server
  
 var http = require('http') 							//#include thư viện http - Tìm thêm về từ khóa http nodejs trên google nếu bạn muốn tìm hiểu thêm. Nhưng theo kinh nghiệm của mình, Javascript trong môi trường NodeJS cực kỳ rộng lớn, khi bạn bí thì nên tìm hiểu không nên ngồi đọc và cố gắng học thuộc hết cái reference (Tài liêu tham khảo) của nodejs làm gì. Vỡ não đó!
 var socketio = require('socket.io')				//#include thư viện socketio
@@ -19,6 +19,6 @@ io.on('connection', function(socket) {
 	//Khi socket client bị mất kết nối thì chạy hàm sau.
 	socket.on('disconnect', function() {
 		console.log("disconnect") 	//in ra màn hình console cho vui
-		clearInterval(interval1)		//xóa chu kỳ nhiệm vụ đi, chứ không xóa là cái task kia cứ chạy mãi thôi đó!
+		//xóa chu kỳ nhiệm vụ đi, chứ không xóa là cái task kia cứ chạy mãi thôi đó!
 	})
 });
